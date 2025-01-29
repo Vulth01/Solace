064ef671846cd801c41338dc1f52b80a1400a354
Vulth01 <damienharwood14@gmail.com>
2025-01-29 23:30:49 +0200

    Added: SolaceEditor || Remove: Any CPU & x86

---
+++ SolaceEditor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SolaceEditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
